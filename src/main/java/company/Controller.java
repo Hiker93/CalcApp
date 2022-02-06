24e541997675b4ea0aabe028028fc85d106e6bad
Mateusz <mat.jozwiak@vp.pl>
2022-02-06 21:13:56 +0100

    refactoring

--- src/main/java/company/Controller.java
+++ src/main/java/company/Controller.java
@@ -1,56 +1,90 @@
 package company;
+
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Controller {
-   View view;
-   Calculator calculator;
-   Item item;
-   public Controller(){this.view = new View();}
+public class Controller implements Runnable {
+    View view;
+    Calculator calculator = new Calculator();
+    Item item;
 
-   public Calculator addItem(){
-      boolean flag = true;
+    public Controller() {
+        this.view = new View(calculator);
+    }
 
-      while(flag) {
-         Scanner scanner = new Scanner(System.in);
-         Scanner scanner1 = new Scanner(System.in);
-         System.out.println("Item name :");
-         String name = scanner.nextLine();
-         System.out.println("Item unit :");
-         String unit = scanner.nextLine();
-         System.out.println("Item quantity :");
-         float quantity = scanner.nextFloat();
-         String answer;
-         System.out.println("jesli to wszystko wpisz Y");
-         answer = scanner1.nextLine();
-         if(answer.equalsIgnoreCase("Y")){
-            flag = false;
-         }
+    private static float getDimensionFromUser() {
+        boolean inputIsNotFloat = true;
+        Scanner scanner = new Scanner(System.in);
+        float value = 0;
+        while(inputIsNotFloat) {
+            try {
+                value = scanner.nextFloat();
+                inputIsNotFloat = false;
+            } catch(InputMismatchException e) {
+                System.out.println("Dear user, i need a number <3");
+                scanner.next();
+            }
+        }
+        return value;
+    }
 
+    public Calculator addItem() {
+        while(continueAddingItems()) {
+            Scanner scanner = new Scanner(System.in);
+            view.getItemNamePrintln();
+            String name = scanner.nextLine();
+            view.getItemUnitPrintln();
+            String unit = scanner.nextLine();
+            float quantity = 0;
+            boolean inputIsNotFloat = true;
+            view.getItemQuantityPrintln();
+            while(inputIsNotFloat) {
+                try {
+                    quantity = scanner.nextFloat();
+                    inputIsNotFloat = false;
+                } catch(InputMismatchException e) {
+                    System.out.println("Dude, quantity has to be a number!");
+                    scanner.nextLine();
+                }
+            }
+            calculator.addToList(name, quantity, unit);
+        }
 
-         view.calculator.addToList(name, quantity, unit);
-      }
+        return calculator;
+    }
 
-      return view.calculator;
+    private boolean continueAddingItems() {
+        String answer;
+        Scanner scanner = new Scanner(System.in);
+        view.getFinishInputPrintln();
+        view.getContinuePrintln();
+        answer = scanner.nextLine();
+        return !answer.equalsIgnoreCase("1");
+    }
 
-   }
+    public Calculator calculatedItem() {
+        calculator.calculateItems(calculator.getItems(), calculator.getRatio());
+        return calculator;
+    }
 
-   public Calculator calculatedItem(){
+    public Calculator setRatio() {
+        view.oldDimensionInfo();
+        float oldDimension = getDimensionFromUser();
+        view.newDimensionInfo();
+        float newDimension = getDimensionFromUser();
+        calculator.calculateRatio(oldDimension, newDimension);
+        return calculator;
+    }
 
-      view.calculator.calculateItems(view.calculator.getItems(), view.calculator.getRatio());
+    @Override
+    public void run() {
+        System.out.println("$");
+        addItem();
+        view.printItem();
+        setRatio();
+        System.out.println(calculator.getRatio());
+        calculatedItem();
+        view.printItem();
 
-      return view.calculator;
-   }
-
-   public Calculator setRatio(){
-      Scanner scanner = new Scanner(System.in);
-      System.out.println("Old dimension :");
-      float oldValue = scanner.nextFloat();
-      System.out.println("New dimension :");
-      float newValue = scanner.nextFloat();
-      view.calculator.calculateRatio(oldValue, newValue);
-
-      return view.calculator;
-   }
-
-
+    }
 }
--- src/main/java/company/Main.java
+++ src/main/java/company/Main.java
@@ -1,19 +1,13 @@
 package company;
 
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
             Controller controller = new Controller();
-            System.out.println("$");
-            controller.addItem();
-            controller.view.printItem();
-//            controller.setRatio();
-//            System.out.println(controller.view.calculator.getRatio());
-//            controller.calculatedItem();
-//            controller.view.printItem();
-
+            controller.run();
     }
 }
 
